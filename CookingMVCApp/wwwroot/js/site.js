3dc36803215951ac1ba43b580f58263388204e69
Justin Chew <jchew1@byu.edu>
2024-02-08 17:46:18 -0700

    Add project files.

---
+++ CookingMVCApp/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#calculateButton').click(function () {
+        var hours = parseFloat($('#hours').val());
+
+        // Validate input, if not then alert user
+        if (isNaN(hours) || hours <= 0) {
+            alert('Please enter a valid positive number for hours.');
+            return;
+        }
+
+        // Calculate hourly rate
+        var hourlyRate = parseFloat($('#hourlyRate').val().replace('$', ''));
+        var total = hours * hourlyRate;
+
+        $('#total').val('$' + total.toFixed(2));
+    });
+});
